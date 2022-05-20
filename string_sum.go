b9d584ad387b6af60777757e5687fee7534cfe68
nastyavolk <nastya.vooolk@gmail.com>
2022-05-20 20:53:04 +0300

    homework-4

--- string_sum.go
+++ string_sum.go
@@ -2,26 +2,45 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
-	// Use when the input is empty, and input is considered empty if the string contains only whitespace
-	errorEmptyInput = errors.New("input is empty")
-	// Use when the expression has number of operands not equal to two
+	errorEmptyInput     = errors.New("input is empty")
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
-
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	input = strings.ReplaceAll(input, " ", "")
+	r := []rune(input)
+	sum := 0
+	operandsCount := 0
+	numbersRegexp := regexp.MustCompile(`\d`)
+	if len(r) == 0 {
+		return "", fmt.Errorf("Error while calculating sum: %w", errorEmptyInput)
+	}
+	for i := 0; i < len(r); i++ {
+		num, _ := strconv.Atoi(string(r[i]))
+		if operandsCount > 2 {
+			return "", fmt.Errorf("Error while calculating sum: %w", errorNotTwoOperands)
+		} else if string(r[i]) == "-" {
+			i++
+			num, _ := strconv.Atoi(string(r[i]))
+			sum -= num
+			operandsCount += 1
+		} else if string(r[i]) == "+" {
+			i++
+			num, _ := strconv.Atoi(string(r[i]))
+			sum += num
+			operandsCount += 1
+		} else if numbersRegexp.MatchString(string(r[i])) {
+			sum += num
+			operandsCount += 1
+		}
+	}
+	output = strconv.Itoa(sum)
+	return output, nil
 }
