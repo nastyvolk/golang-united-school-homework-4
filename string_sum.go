ae053023c45b5786b02d2ffd865f845b6754631c
nastyavolk <nastya.vooolk@gmail.com>
2022-05-21 00:11:12 +0300

    homework-4 fix

--- string_sum.go
+++ string_sum.go
@@ -11,7 +11,6 @@ import (
 var (
 	errorEmptyInput     = errors.New("input is empty")
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
-	invalidOperand      = errors.New("operand contains invalid symbols")
 )
 
 func StringSum(input string) (output string, err error) {
@@ -28,7 +27,8 @@ func StringSum(input string) (output string, err error) {
 	for i := 0; i < len(s); i++ {
 		num, err := strconv.Atoi(s[i])
 		if err != nil {
-			return "", fmt.Errorf("error while calculating sum: %w", invalidOperand)
+			e := err.(*strconv.NumError)
+			return "", fmt.Errorf("error while calculating sum: %w", e.Err)
 		}
 		sum += num
 	}
